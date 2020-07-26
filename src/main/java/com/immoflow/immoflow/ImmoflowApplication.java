2dfb30c847f519a83973413a241111160e9972e3
Etienne Hoven <etienne.hoven@mvise.de>
2020-07-26 13:07:39 +0200

    initial commit

---
+++ src/main/java/com/immoflow/immoflow/ImmoflowApplication.java
@@ -0,0 +1,13 @@
+package com.immoflow.immoflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImmoflowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImmoflowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/immoflow/immoflow/ImmoflowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.immoflow.immoflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImmoflowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
