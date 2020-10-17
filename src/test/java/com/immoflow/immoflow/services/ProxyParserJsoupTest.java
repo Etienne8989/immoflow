ac19dce96592f8323e113cdbc4fedc7b9884bb33
et <et@gmail.com>
2020-10-17 19:31:37 +0200

    fixed test

--- src/test/java/com/immoflow/immoflow/services/ProxyParserJsoupTest.java
+++ src/test/java/com/immoflow/immoflow/services/ProxyParserJsoupTest.java
@@ -1,5 +1,6 @@
 package com.immoflow.immoflow.services;
 
+import com.immoflow.immoflow.resource.ProxyContext;
 import com.immoflow.immoflow.resource.SimpleProxy;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -12,16 +13,16 @@ class ProxyParserJsoupTest {
 
 
     @Test
-    void scrapeProxied() throws IOException, InterruptedException {
+    void scrapeProxied()  {
         ProxyParserJsoup proxyParserJsoup = new ProxyParserJsoup();
-        proxyParserJsoup.scrapeProxies();
+        proxyParserJsoup.scrapeProxies(buildProxyContext());
     }
 
     @Test
     void shouldUseProxyLimit() {
         ReflectionTestUtils.setField(ProxyParserJsoup.class, "workingProxyLimit", 3);
         ProxyParserJsoup  proxyParserJsoup = new ProxyParserJsoup();
-        List<SimpleProxy> simpleProxies   = proxyParserJsoup.scrapeProxies();
+        List<SimpleProxy> simpleProxies   = proxyParserJsoup.scrapeProxies(buildProxyContext());
         Assertions.assertEquals(3,simpleProxies.size());
     }
 
@@ -30,8 +31,14 @@ class ProxyParserJsoupTest {
         ReflectionTestUtils.setField(ProxyParserJsoup.class, "workingProxyLimit", 999);
         ReflectionTestUtils.setField(ProxyParserJsoup.class, "maxActiveThreadNumber", 50);
         ProxyParserJsoup  proxyParserJsoup = new ProxyParserJsoup();
-        List<SimpleProxy> simpleProxies   = proxyParserJsoup.scrapeProxies();
+        List<SimpleProxy> simpleProxies   = proxyParserJsoup.scrapeProxies(buildProxyContext());
         Assertions.assertTrue(simpleProxies.size()>1);
+    }
+
+    ProxyContext buildProxyContext(){
+        ProxyContext proxyContext = new ProxyContext();
+        proxyContext.setSslProxies(true);
+        return proxyContext;
     }
 
 
